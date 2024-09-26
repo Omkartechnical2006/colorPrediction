96601caceb223e1dd4306fa8da9b347114a3575e
Omkartechnical2006 <114220878+Omkartechnical2006@users.noreply.github.com>
2024-09-27 02:43:59 +0530

    Add files via upload

--- public/js/home/script.js
+++ public/js/home/script.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 let slideIndex = 0;
 showSlides();
 
@@ -25,11 +31,33 @@ let wininfoimg1 = document.getElementsByClassName("wininfo")[0];
 let wininfoimg2 = document.getElementsByClassName("wininfo")[1];
 
 setInterval(() => {
-    if(wininfoimg1.style.display==="block"){
-        wininfoimg1.style.display="none";
-        wininfoimg2.style.display="block";
-    }else if(wininfoimg1.style.display==="none"){
-        wininfoimg1.style.display="block";
-        wininfoimg2.style.display="none";
+    if (wininfoimg1.style.display === "block") {
+        wininfoimg1.style.display = "none";
+        wininfoimg2.style.display = "block";
+    } else if (wininfoimg1.style.display === "none") {
+        wininfoimg1.style.display = "block";
+        wininfoimg2.style.display = "none";
     }
 }, 2000);
+
+
+
+//  scripts.js                      
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        document.getElementById('splash-screen').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 1500); // Change this value to adjust how long the splash screen stays
+});
+
+function comingsoon() {
+    let gameMenuHtml = ``;
+    let a = document.getElementsByClassName("game_menu_list")[0];
+    gameMenuHtml = a.innerHTML;
+    a.innerHTML = `<div id="gifContainer" class="gif-container" style="width: 100%;">
+                        <img src="images/comingsoon.gif" alt="Your GIF" width="300px" height="250px"/>
+                    </div>`
+    setTimeout(() => {
+        a.innerHTML = gameMenuHtml;
+    }, 1000);
+}
