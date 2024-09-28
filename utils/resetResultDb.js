2e6ed193a082517a20ff4b66b967413a268378ae
Omkartechnical2006 <114220878+Omkartechnical2006@users.noreply.github.com>
2024-09-28 10:53:35 +0530

    Delete utils directory

--- utils/resetResultDb.js
+++
@@ -1,9 +0,0 @@
-// services/resetResultDb.js
-const WingoBetResult = require('../models/WingoBetResult'); // Adjust the path as needed
-
-async function resetResultDb() {
-    await WingoBetResult.deleteMany({});
-    // console.log("Reset the result db");
-}
-
-module.exports = resetResultDb;
